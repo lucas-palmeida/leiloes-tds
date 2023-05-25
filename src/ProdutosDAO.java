2edfa583ff38d709040438fb05d94bd607f1c4d9
Lucas Pacheco <lucaspacheco95@gmail.com>
2023-05-24 22:41:10 -0300

    feat: finish function connectDB of conectaDAO class and implements cadastrarProduto feature for cadastroVIEW

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,22 +12,29 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-
 
 public class ProdutosDAO {
     
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
+    PreparedStatement ps;
+    ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            ps = conn.prepareStatement("INSERT INTO produtos(nome, valor, status) VALUES (?,?,?)");
+            ps.setString(1, produto.getNome());
+            ps.setInt(2, produto.getValor());
+            ps.setString(3, produto.getStatus());
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não foi possível concluir o cadastro: " + ex.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,10 +1,7 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
-
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -18,16 +15,17 @@ import javax.swing.JOptionPane;
 public class conectaDAO {
     
     public Connection connectDB(){
-        Connection conn = null;
+        String url = "jdbc:mysql://127.0.0.1:3306/uc11";
+        String user = "root";
+        String password = "senha123";
         
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Connection conn = DriverManager.getConnection(url, user, password);
+            return conn;
         }
-        return conn;
+        catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha na conexão com o banco de dados: " + e.getMessage());
+            return null;
+        }
     }
-    
 }
